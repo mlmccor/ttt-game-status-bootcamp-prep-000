b026d3b1aef69e884542d6c6aa56c435ee2f5c9f
Matthew McCormack <mmccormack1182@gmail.com>
2018-08-20 19:24:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,9 +25,9 @@ def won?(board)
     position_3 = board[win_index_3]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      return winner
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      return winner
     else
       return false
     end
